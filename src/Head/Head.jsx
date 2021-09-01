94f5cfb6cc50d5f96acdd7390bc58a88e9dd34d5
mohit49 <mohitanim@gmail.com>
2021-09-01 08:24:43 +0530

    Map Array react

---
+++ src/Head/Head.jsx
@@ -0,0 +1,10 @@
+import Header from '../Tags/Tags'
+import './Head.scss'
+export default function Head() {
+    
+    return(<Header>
+        <div className='wbheader'>
+            <h1>Top Netflix Movies</h1>
+        </div>
+        </Header>)
+}
